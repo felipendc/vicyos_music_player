1d94e4b30a32277f60014c00e1baaf5b6666c72a
vicyoslife <felipendc10phoenix@gmail.com>
2024-05-27 08:32:29 -0300

    Switching to CTK and enable dark mode

--- V2/main.pyw
+++ V2/main.pyw
@@ -1,13 +1,13 @@
 # main.py
-import tkinter as tk
-from tkinter import filedialog
+import customtkinter as ctk # pip3 install customtkinter, pip install --upgrade pip
+from customtkinter import filedialog
 import music_player
 
 # Initialize the music player
 music_player.init_music_player()
 
 def on_select_folder():
-    music_player.selected_folder(filedialog, play_pause_button, stop_button, next_button, previous_button, is_play_button, repeat_button)
+    music_player.selected_folder(filedialog, play_pause_button, stop_button, next_button, previous_button, is_play_button, repeat_button, select_folder_button)
 
 def on_play_or_pause():
     music_player.on_button_play_or_pause(play_pause_button)
@@ -28,29 +28,31 @@ def on_repeat_button_label():
     music_player.repeat_button_label(repeat_button)
 
 
-root = tk.Tk()
+ctk.set_appearance_mode("dark") # Set dark mode theme globally
+root = ctk.CTk()
 root.title("Vicyos Music Player")
 root.geometry("300x470")
 
-play_pause_button = tk.Button(root, text="Start Playing the song", command=on_play_or_pause, state=tk.DISABLED)
+
+play_pause_button = ctk.CTkButton(root, fg_color="#444444", hover_color="#555555", text="Start Playing the song", command=on_play_or_pause, state=ctk.DISABLED)
 play_pause_button.pack(pady=20)
 
-stop_button = tk.Button(root, text="Stop", command=on_stop, state=tk.DISABLED)
+stop_button = ctk.CTkButton(root, fg_color="#444444", hover_color="#555555", text="Stop", command=on_stop, state=ctk.DISABLED)
 stop_button.pack(pady=20)
 
-is_play_button = tk.Button(root, text="Is there anything playing?", command=on_check_music, state=tk.DISABLED)
+is_play_button = ctk.CTkButton(root, fg_color="#444444", hover_color="#555555", text="Is there anything playing?", command=on_check_music, state=ctk.DISABLED)
 is_play_button.pack(pady=20)
 
-select_folder_button = tk.Button(root, text="Open a folder", command=on_select_folder)
+select_folder_button = ctk.CTkButton(root, fg_color="#666666", hover_color="#777777", text="Open a folder", command=on_select_folder)
 select_folder_button.pack(pady=20)
 
-next_button = tk.Button(root, text="Previous", command=on_previous_song, state=tk.DISABLED)
+next_button = ctk.CTkButton(root, fg_color="#444444", hover_color="#555555", text="Previous", command=on_previous_song, state=ctk.DISABLED)
 next_button.pack(pady=20)
 
-previous_button = tk.Button(root, text="Next", command=on_next_song, state=tk.DISABLED)
+previous_button = ctk.CTkButton(root, fg_color="#444444", hover_color="#555555", text="Next", command=on_next_song, state=ctk.DISABLED)
 previous_button.pack(pady=20)
 
-repeat_button = tk.Button(root, text="Repeat mode: Off", command=on_repeat_button_label, state=tk.DISABLED)
+repeat_button = ctk.CTkButton(root, fg_color="#444444", hover_color="#555555", text="Repeat mode: Off", command=on_repeat_button_label, state=ctk.DISABLED)
 repeat_button.pack(pady=20)
 
 root.after(1000, lambda: music_player.repeat_checker(root, play_pause_button))
--- V2/music_player.py
+++ V2/music_player.py
@@ -1,13 +1,15 @@
 # music_player.py
 import pygame
 import os
-import tkinter as tk
+import customtkinter as ctk
+
 
 repeat_song = "none"
 pause_music = False
 stop_music = False
 music_folder_path = []
 song_index = 0
+
 
 repeat_options = {
     'NONE': "none",
@@ -21,9 +23,8 @@ def init_music_player():
     pygame.mixer.init()
 
 
-def selected_folder(filedialog, play_pause_button, stop_button, next_button, previous_button, is_play_button, repeat_button):
+def selected_folder(filedialog, play_pause_button, stop_button, next_button, previous_button, is_play_button, repeat_button, select_folder_button):
     global music_folder_path, song_index
-
     directory = filedialog.askdirectory()
     if directory:
         music_folder_path.clear()  # Empty the current playlist
@@ -32,20 +33,20 @@ def selected_folder(filedialog, play_pause_button, stop_button, next_button, pre
                 if os.path.splitext(file)[1] == '.mp3':
                     path = (root_ + '/' + file).replace('\\', '/')
                     music_folder_path.append(path)
-
         if len(music_folder_path) > 0:
             song_index = 0
             on_button_stop(play_pause_button)
             on_button_play_or_pause(play_pause_button)
-            play_pause_button.config(state=tk.NORMAL)
-            stop_button.config(state=tk.NORMAL)
-            next_button.config(state=tk.NORMAL)
-            previous_button.config(state=tk.NORMAL)
-            is_play_button.config(state=tk.NORMAL)
-            repeat_button.config(state=tk.NORMAL)
-
+            play_pause_button.configure(state=ctk.NORMAL)
+            stop_button.configure(state=ctk.NORMAL)
+            next_button.configure(state=ctk.NORMAL)
+            previous_button.configure(state=ctk.NORMAL)
+            is_play_button.configure(state=ctk.NORMAL)
+            repeat_button.configure(state=ctk.NORMAL)
+            select_folder_button.configure(fg_color="#444444", hover_color="#555555",)
     else:
         pass
+
 
 def next_song():
     global song_index
@@ -70,31 +71,29 @@ def previous_song():
 
 def on_button_play_or_pause(play_pause_button):
     global pause_music, music_folder_path, stop_music
-
     if not pygame.mixer.music.get_busy() and pause_music == False:
         pygame.mixer.music.load(music_folder_path[song_index])
         pygame.mixer.music.play()
         stop_music = False
-        play_pause_button.config(text="Pause")
+        play_pause_button.configure(text="Pause")
     elif pause_music == False:
         pygame.mixer.music.pause()
         pause_music = True
-        play_pause_button.config(text="Play")
+        play_pause_button.configure(text="Play")
     elif pause_music == True:
         pygame.mixer.music.unpause()
         pause_music = False
-        play_pause_button.config(text="Pause")
+        play_pause_button.configure(text="Pause")
 
 def on_button_stop(play_pause_button):
     global pause_music, stop_music
     pygame.mixer.music.stop()
     pause_music = False
     stop_music = True
-    play_pause_button.config(text="Start Playing the song")
+    play_pause_button.configure(text="Start Playing the song")
 
 def check_music():
     global music_folder_path
-
     if not pygame.mixer.music.get_busy() and pause_music == False:
         print("There is nothing being played at the moment...")
     elif not pygame.mixer.music.get_busy() and pause_music == True:
@@ -102,9 +101,9 @@ def check_music():
     else:
         print("There is a song being played at the moment...")
 
+
 def repeat_checker(root, play_pause_button):
     global music_folder_path, repeat_song
-
     if not pygame.mixer.music.get_busy() and pause_music == False:
         if repeat_song == "playlist":
             if len(music_folder_path) > 0 and not stop_music:
@@ -117,8 +116,8 @@ def repeat_checker(root, play_pause_button):
             print(repeat_song) # Printing to debug the code!
         elif repeat_song == "none":
             if len(music_folder_path) > 0:
-                play_pause_button.config(text="Play")
-                # play_pause_button.config(text="Play")
+                play_pause_button.configure(text="Play")
+                # play_pause_button.configure(text="Play")
                 pass
             print(repeat_song) # Printing to debug the code!
     root.after(1000, lambda: repeat_checker(root, play_pause_button))
@@ -126,21 +125,18 @@ def repeat_checker(root, play_pause_button):
 
 def repeat_button_label(repeat_button):
     global repeat_song
-
     if repeat_song == repeat_options["NONE"]:
         print(repeat_song) # Printing to debug the code!
-        repeat_button.config(text="Repeat: Current song")
+        repeat_button.configure(text="Repeat: Current song")
         repeat_song = repeat_options["CURRENT"]
         print(repeat_song) # Printing to debug the code!
-
     elif repeat_song == repeat_options["CURRENT"]:
         print(repeat_song) # Printing to debug the code!
-        repeat_button.config(text="Repeat: Playlist")
+        repeat_button.configure(text="Repeat: Playlist")
         repeat_song = repeat_options["PLAYLIST"]
         print(repeat_song) # Printing to debug the code!
-
     elif repeat_song == repeat_options["PLAYLIST"]:
         print(repeat_song) # Printing to debug the code!
-        repeat_button.config(text="Repeat mode: Off")
+        repeat_button.configure(text="Repeat mode: Off")
         repeat_song = repeat_options["NONE"]
         print(repeat_song) # Printing to debug the code!
